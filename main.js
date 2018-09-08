84a536b817fd0047fd5eaf39c82ce24c8c4422fe
Mary Rys <rys.mary@gmail.com>
2018-09-08 14:21:00 -0500

    spanish button working

--- main.js
+++ main.js
@@ -1,31 +1,52 @@
 const spanish = {
-    "merry": "Feliz"
-    "christmas": "Navidad"
-    "and": "y"
-    "happy": "prospero"
-    "new: "nuevo"
+    "merry": "Feliz",
+    "christmas": "Navidad",
+    "and": "y",
+    "happy": "prospero",
+    "new": "nuevo",
     "year": "ano"
-}
+};
 
 const german = {
-    "merry": "Frohe"
-    "christmas": "Weihnachten"
-    "and": "und ein"
-    "happy": "gluckliches"
-    "new": "Neues"
+    "merry": "Frohe",
+    "christmas": "Weihnachten",
+    "and": "und ein",
+    "happy": "gluckliches",
+    "new": "Neues",
     "year": "Jahr"
-}
+};
 
 const french = {
-    "merry": "joyeux"
-    "christmas": "Noel"
-    "and": "et"
-    "happy: "bonne"
-    "new": "nouvel"
+    "merry": "joyeux",
+    "christmas": "Noel",
+    "and": "et",
+    "happy": "bonne",
+    "new": "nouvel",
     "year": "An"
-}
+};
 
 const printToDom = (stringToPrint, divId) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML += stringToPrint;
 };
+      
+const spanishTranslate = () => {
+    const inputValue = document.getElementById('inputBox').value;
+    const inputWords = inputValue.split(" ");
+    // for (let i = 0; i < inputWords.length; i++){
+    //     spanish[inputWords[i]];
+    // }
+    const spanishTranslatedString = spanishStringBuilder (inputWords);
+    printToDom(spanishTranslatedString, 'returnText');
+}
+
+const spanishStringBuilder  = (inputWords) => {
+    let newString = '';
+    for(let i = 0; i < inputWords.length; i++) {
+        let spanishWord = spanish[inputWords[i]];
+        newString += `<p>${spanishWord}</p>`
+    };
+    return newString;
+}
+const spanishButton = document.getElementById('spanishButton');
+    spanishButton.addEventListener("click", spanishTranslate);
