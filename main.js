05bbb705b8365713e0b474b9be2005554b7e9b20
Mary Rys <rys.mary@gmail.com>
2018-09-09 14:36:38 -0500

    Three language buttons working

--- main.js
+++ main.js
@@ -50,3 +50,39 @@ const spanishStringBuilder  = (inputWords) => {
 }
 const spanishButton = document.getElementById('spanishButton');
     spanishButton.addEventListener("click", spanishTranslate);
+
+    const germanTranslate = () => {
+        const inputValue = document.getElementById('inputBox').value;
+        const inputWords = inputValue.split(" ");
+        const germanTranslatedString = germanStringBuilder (inputWords);
+        printToDom(germanTranslatedString, 'returnText');
+    }
+    
+    const germanStringBuilder  = (inputWords) => {
+        let newString = '';
+        for(let i = 0; i < inputWords.length; i++) {
+            let germanWord = german[inputWords[i]];
+            newString += `<p>${germanWord}</p>`
+        };
+        return newString;
+    }
+    const germanButton = document.getElementById('germanButton');
+        germanButton.addEventListener("click", germanTranslate);
+
+        const frenchTranslate = () => {
+            const inputValue = document.getElementById('inputBox').value;
+            const inputWords = inputValue.split(" ");
+            const frenchTranslatedString = frenchStringBuilder (inputWords);
+            printToDom(frenchTranslatedString, 'returnText');
+        }
+        
+        const frenchStringBuilder  = (inputWords) => {
+            let newString = '';
+            for(let i = 0; i < inputWords.length; i++) {
+                let frenchWord = french[inputWords[i]];
+                newString += `<p>${frenchWord}</p>`
+            };
+            return newString;
+        }
+        const frenchButton = document.getElementById('frenchButton');
+            frenchButton.addEventListener("click", frenchTranslate);
